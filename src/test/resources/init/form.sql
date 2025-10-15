7dc04f4f3d2b92d978e55382e95c7b1850cb9f26
T.C. <tonny_1983@hotmail.com>
2025-10-15 18:45:23 +0800

    upgrade Camunda version to 7.24.0 (final official supported version)

--- src/test/resources/init/form.sql
+++ src/test/resources/init/form.sql
@@ -1,36 +1,18 @@
 create table if not exists ENHACEMENT_FORM
 (
-    ID
-    varchar
-(
-    64
-) not null primary key,
-    NAME_TXT varchar
-(
-    64
-) not null,
-    TENANT_TXT varchar
-(
-    64
-) default null,
-    TYPE_TXT varchar
-(
-    16
-) default '',
+    ID varchar(64) not null primary key,
+    NAME_TXT varchar(64) not null,
+    TENANT_TXT varchar(64) default null,
+    TYPE_TXT varchar(16) default '',
+    DEFINITION_KEY_TXT VARCHAR(255) default '',
     FROM_DATA text default '',
     CREATED_TS bigint,
     UPDATED_TS bigint,
-    CREATE_BY_TXT varchar
-(
-    20
-) default null,
-    UPDATED_BY_TXT varchar
-(
-    20
-) default null,
+    CREATE_BY_TXT varchar(20) default null,
+    UPDATED_BY_TXT varchar(20) default null,
     DELETE_FG smallint default 0
-    );
+);
 
 MERGE INTO ENHACEMENT_FORM
-    (id, name_txt, tenant_txt, type_txt, from_data, created_ts, updated_ts, create_by_txt, updated_by_txt, delete_fg)
-    VALUES ('324234234234', 'form1', NULL, 'from', '[{"key": "value"}]', NULL, NULL, NULL, NULL, 0);
+    (id, name_txt, tenant_txt, type_txt, DEFINITION_KEY_TXT, from_data, created_ts, updated_ts, create_by_txt, updated_by_txt, delete_fg)
+    VALUES ('324234234234', 'form1', NULL, 'from', '324234234234', '[{"key": "value"}]', NULL, NULL, NULL, NULL, 0);
